05f9a1fc638dc6859b5a74b6342b8d012e050502
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-18 18:13:55 +0600

    json data & banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className="w-100">
+            <Image src="https://raw.githubusercontent.com/aduyti/my-test-images/main/service-images/banner.png" alt="" fluid />
         </div>
     );
 };
