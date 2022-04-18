4cfc2450b4c403fbf3bc0143aa03659d34ec2c62
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-18 15:57:22 +0600

    responsive navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <></>
-        );
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">CODE TUTOR</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="about">About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to="login">Login</Nav.Link>
+                        <Nav.Link as={Link} to="register">Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Header;
