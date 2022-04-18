0cd7c48419a93d4b7cb148a6dbb0a6c8d6f184f9
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-18 16:51:41 +0600

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
-        <div>
-            fo
+        <div className="mt-5 p-3 bg-light">
+            <Container>
+                <Row style={{ color: "darkgray" }}>
+                    <Col md={10} className="my-auto">Copyright &copy; {year} Code Tutor</Col>
+                    <Col md={2} className="text-md-start py-3">
+                        <p>Contacts:</p>
+                        <p>+010101001010</p>
+                        <p>tutor@codetutor.edu</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
