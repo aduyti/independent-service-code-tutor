0a6db8813da5fb7968a06d82a7f910ffdfb3e8f6
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-19 23:50:50 +0600

    checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Checkout = () => {
+    const navigate = useNavigate();
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        navigate('/');
+    }
     return (
         <div className="container">
-            Checkout
+            <h1 className="text-primary text-center">Fill this Form to Book</h1>
+
+            <Form className="w-50 mx-auto" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="email" placeholder="Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Your textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
+                    <Form.Label>Your Extensions</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="w-100">
+                    Submit
+                </Button>
+
+            </Form>
         </div>
     );
 };
